d7666ffc07272d0b542686ca9e501c63fe41353a
alex.work.04.26@gmail.com <alex.work.04.26@gmail.com>
2025-03-02 13:29:19 +0300

    completed lesson №38

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest(){
+
+        Assertions.assertTrue(1==1);
+    }
+
+}
